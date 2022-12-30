9afd8051753bb5510e2c0ccf384bb613cba145d8
Manuel Murillo <manuel1307ms@gmail.com>
2022-12-30 17:45:55 -0600

    Agregando mascota

---
+++ src/main/java/modelo/Mascota.java
@@ -0,0 +1,19 @@
+package modelo;
+
+public class Mascota {
+	
+	private String nombre;
+	
+	public Mascota(String nombre) {
+		this.nombre=nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
